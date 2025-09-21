852f57b61241b692efc5d8c5c638dac356ea1e77
paffexe <kamolov607@gmail.com>
2025-09-21 19:10:16 +0500

    qwe

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,24 +1,25 @@
-import { useQuery, useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { memo } from 'react';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { memo } from "react";
 
 const About = () => {
-  const {data , isLoading, error, isError, isSuccess} = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["recipe"],
-    queryFn: () => axios.get("https://dummyjson.com/recipes").then(res => res.data),
-  })
+    queryFn: () =>
+      axios.get("https://dummyjson.com/recipes").then((res) => res.data),
+  });
 
   return (
     <div className="About">
-       {isLoading && <h1>Laoding...</h1>}
-       <div>
-        {data?.recipes?.map((item:any) => (
+      {isLoading && <h1>Laoding...</h1>}
+      <div>
+        {data?.recipes?.map((item: any) => (
           <div key={item.id}>
             <h3>{item.name}</h3>
             <hr />
           </div>
         ))}
-       </div>
+      </div>
     </div>
   );
 };
